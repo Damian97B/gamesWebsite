a05816ed1f40353ba6c2053e5446fb660a578a53
Damian97B <damian497k@gmail.com>
2022-07-09 16:53:20 +0200

    add react to project

---
+++ GamesBox.js
@@ -0,0 +1,54 @@
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+  
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   root.render(element);
+// }
+
+// setInterval(tick, 1000);
+
+
+// const container2 = ReactDOM.createRoot(document.getElementById('container2'));
+// const test = <h2>test</h2>;
+// function box(){
+//     const element = (
+//         <div class = "all">
+//             <div class="container">
+        
+//                 <div class="row" id="row1">
+//                     <h1>Druga gra</h1>
+//                     <div class="col-sm-12">
+        
+//                     </div>
+//                 </div>
+//             </div>
+//             <div class="container">
+//                 <div class="row">
+//                     <div class="col-sm-6">
+//                         <div class="nameImage">
+//                             <h2>Państwa miasta</h2>
+//                         </div>
+//                         <a href="#">
+//                         <img src="https://cdn.pixabay.com/photo/2015/09/17/15/44/game-944386__480.jpg" alt=""> </img>
+//                         <br></br> <h3>Kliknij aby zagrać</h3></a>
+//                     </div>
+//                     <div class="col-sm-6">
+//                         <p>Typowa gra w kółko krzyżyk na stronie internetowej, graj razem ze znajomym, pokaż kto jest lepszy.</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+        
+// }
+// container.render(element);
